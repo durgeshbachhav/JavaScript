d6b72495c2865323d42cf27e363cfa489607ae34
durgeshbachhav <bachhavdurgesh123@gmail.com>
2023-09-13 17:02:46 +0530

    comparison and dataType summery

---
+++ Basic/dataTypes-summery.js
@@ -0,0 +1,30 @@
+// primitives
+
+// 7 types: String, Number, Boolean, Undefined, Null , Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = true;
+const outsidetemp = null
+let userEmail 
+// how to use symbol
+const id =Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id == anotherId);
+
+
+
+// reference datatypes
+// Array, objects, Functions
+const heros = ['spiderman', 'ironman','batman','captain america'];
+
+const myObj={
+     username:"durgesh",
+     age:22,
+}
+
+const myFun = function () {
+     console.log('hello world');
+     
+}
