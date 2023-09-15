af97d6fdd2fdcca9eb0036aeeaf105a086b47086
durgeshbachhav <bachhavdurgesh123@gmail.com>
2023-09-15 08:47:47 +0530

    add do, while loops

---
+++ 05_Loops/Two.js
@@ -0,0 +1,32 @@
+// while and do-while loop
+
+
+// same as if statement
+// declare
+// let i=0;
+// // condition
+// while (i<=10) {
+//      console.log(`value of index is ${i}`);
+//      // updation
+//      i+=2;
+     
+// }
+
+// let MyArray = [ 'flash','ironman','sprderman']
+
+// let arr = 0;
+// while (arr < MyArray.length) {
+//      console.log(`value is ${MyArray[arr]}`);
+//      arr++;
+// }
+
+//do-while loop
+
+// let score = 11;
+// do {
+//      console.log(`score is ${score}`);
+//      score++;
+// } while (score<=10);
+
+// it is udhari system pehle karid lo baad mein paise do
+
